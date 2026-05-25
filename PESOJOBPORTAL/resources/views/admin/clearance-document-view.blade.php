58fa42771cd52aa1cfb2476509c2691c1d161163
Lord-binns <vince.olemberio2004@gmail.com>
2026-05-25 16:32:05 +0800

    upadted image layout

--- PESOJOBPORTAL/resources/views/admin/clearance-document-view.blade.php
+++ PESOJOBPORTAL/resources/views/admin/clearance-document-view.blade.php
@@ -277,7 +277,7 @@
                 <div class="header-line">Province of Bukidnon</div>
                 <div class="header-line">MUNICIPALITY OF MANOLO FORTICH</div>
                 <div class="header-line peso-office">PUBLIC EMPLOYMENT SERVICE OFFICE</div>
-                <img src="data:image/png;base64,{{ base64_encode(file_get_contents(resource_path('logos/ip-img.png'))) }}" alt="Divider" style="width: 100%; max-width: 400px; height: auto; margin: 0;">
+                <img src="data:image/png;base64,{{ base64_encode(file_get_contents(resource_path('logos/ip-img.png'))) }}" alt="Divider" style="width: 100%; max-width: 400px; height: auto; margin-top: -0.2rem;">
             </div>
             
             <div class="title">PESO CLEARANCE</div>
