90bc639b3a840ef1d3b12145bb63e0d7cd903f60
Lord-binns <vince.olemberio2004@gmail.com>
2026-05-26 10:45:31 +0800

    Moved divider image higher

--- PESOJOBPORTAL/resources/views/admin/clearance-document-view.blade.php
+++ PESOJOBPORTAL/resources/views/admin/clearance-document-view.blade.php
@@ -277,7 +277,7 @@
                 <div class="header-line">Province of Bukidnon</div>
                 <div class="header-line">MUNICIPALITY OF MANOLO FORTICH</div>
                 <div class="header-line peso-office">PUBLIC EMPLOYMENT SERVICE OFFICE</div>
-                <img src="data:image/png;base64,{{ base64_encode(file_get_contents(resource_path('logos/ip-img.png'))) }}" alt="Divider" style="width: 100%; max-width: 400px; height: auto; margin-top: -0.5rem;">
+                <img src="data:image/png;base64,{{ base64_encode(file_get_contents(resource_path('logos/ip-img.png'))) }}" alt="Divider" style="width: 100%; max-width: 400px; height: auto; margin-top: -1rem;">
             </div>
             
             <div class="title">PESO CLEARANCE</div>
